1f306ab67073c9cc2f445492cce7380e6d47bb54
Heli Sivunen <heli@mooncat.tv>
2020-10-13 09:18:26 +0100

    Test passing.

--- src/TopStories.test.js
+++ src/TopStories.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen, waitForElement } from '@testing-library/react'
 
 import TopStories from './TopStories'
-import mockResponse from './__mocks__/nytimes_response.json'
+import mockResponse from './__mocks__/nytimes-response.json'
 
 // mock fetch and return the mock response Json from __mocks__
 jest.spyOn(global, "fetch").mockImplementation(() => {
@@ -14,7 +14,7 @@ jest.spyOn(global, "fetch").mockImplementation(() => {
 test("shows top stories", async () => {
   render(<TopStories />)
 
-  const element = await waitForElement(() => (screen.getByText("I AM A TEST RESPONSE"))
+  const element = await waitForElement(() => screen.getByText("I AM A TEST RESPONSE"))
 
   screen.debug()
   expect(element).toBeInTheDocument()
